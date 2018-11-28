817b9deed1f54b03c7d0620010e35fab52b988b0
Trinh Duc Tai <trinhductai6869@gmail.com>
2018-11-28 20:19:05 +0700

    add signup of Minh

---
+++ front-end/homescreen/homescreen.js
@@ -0,0 +1,14 @@
+// GENERAL SETTING
+window.sr = ScrollReveal({ reset: true });
+
+// Custom Settings
+sr.reveal('.foo-1', { duration: 1000 });
+
+sr.reveal('.foo-2', {
+    origin: 'right',
+    duration: 2000
+});
+
+sr.reveal('.foo-3', {
+    duration: 2000
+});
